216de718bc97bbdfee781bfd1fa992c8b4c85676
Marcos Yudi <marcosyudi96@hotmail.com>
2023-05-04 00:40:43 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import TopButtons from "./components/TopButtons";
+import Inputs from "./components/Inputs";
+import TimeAndLocation from "./components/TimeAndLocation";
+import TemperatureAndDetailes from "./components/TemperatureAndDetailes";
+//import Forecast from "./components/Forecast";
+import getFomattedWeatherData from "./services/weatherService";
 
 function App() {
+  const [query, setQuery] = useState({ q: "tokyo" });
+  const [weather, setWeather] = useState("");
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      await getFomattedWeatherData({ ...query }).then((data) => {
+        setWeather(data);
+      });
+    };
+
+    fetchWeather();
+  }, [query, weather]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br
+     from-cyan-700 to-blue-700 shadow-xl shadow-gray-400"
+    >
+      <TopButtons />
+      <Inputs />
+
+      <TimeAndLocation weather={weather} />
+      <TemperatureAndDetailes weather={weather} />
+      {/*
+      <Forecast title="hourly forecast" />
+      <Forecast title="daily forecast" />
+      */}
     </div>
   );
 }
